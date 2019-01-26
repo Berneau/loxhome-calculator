bdd185efbc0845a1dc40f21497297cc1aa479da9
berneau <bernhard.steger@gmail.com>
2019-01-26 14:15:51 +0700

    finished slots-to-sku calculation

--- ajax/calculate-rooms.php
+++ ajax/calculate-rooms.php
@@ -6,10 +6,26 @@ function lxhm_calculate_rooms() {
   $decodedData = lxhm_decode_data($_POST['formData']);
   if (!$decodedData) return;
 
-  // lxhm_get_server_type_sku($decodedData->serverType);
+  $server_sku = lxhm_get_server_type_sku($decodedData->serverType);
   $rooms_skus = lxhm_loop_over_rooms($decodedData->rooms);
   
-  print_r($rooms_skus);
+  $overall_skus = array();
+  
+  // add server to overall list of skus
+  $overall_skus[$server_sku] = 1;
+  
+  // add new items to overall list of skus
+  foreach ($rooms_skus->add as $key => $value) {
+    $overall_skus[$key] = $value;
+  }
+  
+  // check if slots are exceeding capacity -> add extras if necessary
+  foreach ($rooms_skus->slots as $key => $value) {
+    $amount = lxhm_get_amount_by_slots($key, $value);
+    $overall_skus[$key] = $amount;
+  }
+  
+  print_r($overall_skus);
   
   wp_die();
 }
@@ -20,26 +36,43 @@ function lxhm_decode_data($data) {
   return json_decode($unslashedData);
 }
 
-// function lxhm_get_server_type_sku($type) {
-//   if ($data->serverType == 'miniserver') return array('100001' => 1);
-//   if ($data->serverType == 'miniserver-go') return array('100139' => 1);
-// }
-
+function lxhm_get_server_type_sku($type) {
+  if ($type == 'miniserver-go') return '100139';
+  return '100001';
+}
 
 function lxhm_loop_over_rooms($rooms) {
   $sku_to_add = array();
   $sku_for_slot = array();
   
   foreach ($rooms as $room) {
-  
     foreach ($room->articles as $article) {
   
       $requirements = lxhm_request_skus_from_article($article->type, $article->option);
+      
+      // handle slots needed
+      for ($i = 0; $i < sizeof($requirements->slots); $i++) {
+        foreach ($requirements->slots[$i] as $key => $value) {
+          if (!isset($sku_for_slot[$key])) $sku_for_slot[$key] = 0;
+          $sku_for_slot[$key] += ($value * $article->amount);
+        }
+      }
+      
+      // handle items to add
+      for ($i = 0; $i < sizeof($requirements->new); $i++) {
+        foreach ($requirements->new[$i] as $key => $value) {
+          if (!isset($sku_to_add[$key])) $sku_to_add[$key] = 0;
+          $sku_to_add[$key] += ($value * $article->amount);
+        }
+      }
+
     }
-  
   }
   
-  return $requirements;
+  $return_obj = new stdClass();
+  $return_obj->slots = $sku_for_slot;
+  $return_obj->add = $sku_to_add;
+  return $return_obj;
 }
 
 
--- includes/product-slots.php
+++ includes/product-slots.php
@@ -1,14 +1,37 @@
 <?php
-  define('RELAY_EXTENSION', 14);
-  define('AIR_BASE_EXTENSION', 120);
-  define('DI_EXTENSION', 20);
-  define('TREE_EXTENSION', 90);
-  define('NANO_DI_TREE', 6);
-  define('EXTENSION', 8);
-  define('12_KANAL_VERSTAERKER', 12);
-  define('DIMMER_EXTENESION', 14);
-  define('RGBW_24V_DIMMER', 4);
-  define('LED_SPOT_WW_V2', 10);
+  // define('100038', 14);
+  // define('100114', 120);
+  // define('100283', 20);
+  // define('100218', 90);
+  // define('100242', 6);
+  // define('100002', 8);
+  // define('200110', 12);
+  // define('100029', 14);
+  // define('rgbw-24v-dimmer', 4);
+  // define('led-spot-ww', 10);
+  
+  function lxhm_get_amount_by_slots($sku, $amount) {
+    $slots = array(
+      '100038' => 14,
+      '100114' => 120,
+      '100283' => 20,
+      '100218' => 90,
+      '100242' => 6,
+      '100002' => 8,
+      '200110' => 12,
+      '100029' => 14,
+      'rgbw-24v-dimmer' => 4,
+      'led-spot-ww' => 10
+    );
+    
+    // amount does not exceed slot limit
+    if ($slots[$sku] >= $amount) return 1;
+    
+    $base = intdiv($amount, $slots[$sku]);
+    $remainder = $amount % $slots[$sku];
+    if ($remainder != 0) $base++;
+    return $base;
+  }
   
   function lxhm_request_skus_from_article($type, $option) {
     $relations_string = file_get_contents(LXHM_PLUGIN_DIR . '/includes/sku-relations.json');
