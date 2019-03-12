d58dc5c4bb804d7199221c293f52ca624e1eabfc
berneau <bernhard.steger@gmail.com>
2019-03-12 12:58:04 +0100

    bump version to 1.8

--- lxhm-calculator.php
+++ lxhm-calculator.php
@@ -3,7 +3,7 @@
   Plugin Name: Loxhome Calculator
   Plugin URI: 
   description: A calculator for Loxhome products.
-  Version: 1.7
+  Version: 1.8
   Author: Bernhard Steger
   Author URI: https://berneau.at
   License: GPL2
